623747415c34ea8ff4c8041dcf0c6d2e77381196
Greg De La Torre <gdlt5@nmsu.edu>
2019-03-11 17:00:47 -0600

    Added Platform & ButterBall Prototypes

---
+++ Baby_Fighter_Ultra/Assets/Scripts/playerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    //private Rigidbody2D rb;
+    public GameObject player;
+    public float speed;
+    Transform trans;
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Awake(){
+		trans = GetComponent<Transform> ();
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if(Input.GetKey(KeyCode.A)){
+        	//Vector3 movement = new Vector3(10,0,0);
+        	rb.AddTorque(10);
+
+        }
+        else if(Input.GetKey(KeyCode.D)){
+        	//Vector3 movement = new Vector3(10,0,0);
+        	rb.AddTorque(-10);
+
+        }
+        else
+        	rb.velocity = Vector3.zero;
+	}
+}
