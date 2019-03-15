3c97c6919544ed62113a0e763b1610285392ef80
Greg De La Torre <gdlt5@nmsu.edu>
2019-03-15 13:39:55 -0600

    Fixed delay between audio files
    Audio files set to play one after another using script now have no delay between one another

--- Baby_Fighter_Ultra/Assets/Scripts/startScreenAudioManager.cs
+++ Baby_Fighter_Ultra/Assets/Scripts/startScreenAudioManager.cs
@@ -1,12 +1,19 @@
 ﻿ using UnityEngine;
  using System.Collections;
- 
+
+[RequireComponent(typeof(AudioSource))]
 public class startScreenAudioManager: MonoBehaviour
 {
      public AudioSource audioSourceIntro;
      public AudioSource audioSourceLoop;
      private bool startedLoop;
  
+
+    void Start(){
+        audioSourceIntro.Play();
+        audioSourceLoop.PlayDelayed(audioSourceIntro.clip.length-1.5f);
+    }
+    /*
      void FixedUpdate()
      {
          if (!audioSourceIntro.isPlaying && !startedLoop)
@@ -16,4 +23,5 @@ public class startScreenAudioManager: MonoBehaviour
              startedLoop = true;
          }
      }
+     */
  }
