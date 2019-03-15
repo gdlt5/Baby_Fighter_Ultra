2b264a6a08e0225267a8194f6acd31d8b4c827fe
Greg De La Torre <gdlt5@nmsu.edu>
2019-03-14 22:09:08 -0600

    Added Controller Support for Right-Side Player
    Right-player faces towards left-player at all time, controls oriented to other player.

--- Baby_Fighter_Ultra/Assets/Scripts/playerController2.cs
+++ Baby_Fighter_Ultra/Assets/Scripts/playerController2.cs
@@ -7,34 +7,76 @@ public class playerController2 : MonoBehaviour {
     public GameObject player;
     public float speed;
     private bool isGrounded = true;
-
+    Transform target;
+    float someScale;
     // Use this for initialization
-    
+    void Start(){
+
+    	target = GameObject.FindWithTag("Player1").transform;
+    	someScale = transform.localScale.x;
+
+    }
     
     // Update is called once per frame
     void FixedUpdate () {
+        if (transform.position.x > target.position.x){
+	        if(Input.GetAxis("P2Hor") > 0.5){
 
-        if(Input.GetAxis("P2Hor") > 0.5){
+	            Vector2 movingVector = new Vector2(Input.GetAxis("P2Hor") * (speed/4) * Time.deltaTime, Input.GetAxis("P2Vert")* 0 * Time.deltaTime);
 
-            Vector2 movingVector = new Vector2(Input.GetAxis("P2Hor") * speed * Time.deltaTime, Input.GetAxis("P2Vert")* 0 * Time.deltaTime);
+	            player.transform.Translate(movingVector.x, 0f, 0f);
+	        }
 
-            player.transform.Translate(movingVector.x, 0f, 0f);
-        }
+	        if(Input.GetAxis("P2Hor") < -0.5){
 
-        if(Input.GetAxis("P2Hor") < -0.5){
+	            Vector2 movingVector = new Vector2(Input.GetAxis("P2Hor") * speed * Time.deltaTime, Input.GetAxis("P2Vert")* 0 * Time.deltaTime);
 
-            Vector2 movingVector = new Vector2(Input.GetAxis("P2Hor") * (speed/4) * Time.deltaTime, Input.GetAxis("P2Vert")* 0 * Time.deltaTime);
-
-            player.transform.Translate(movingVector.x, 0f, 0f);
-        }
+	            player.transform.Translate(movingVector.x, 0f, 0f);
+	        }
 
 
-        if(Input.GetAxis("P2Vert") == 1){
-            if(isGrounded){
-                isGrounded = false;
-                GetComponent<Rigidbody2D>().AddForce(new Vector2(0,10), ForceMode2D.Impulse);
-            }
-        }
+	        if(Input.GetAxis("P2Vert") == 1){
+	            if(isGrounded){
+	                isGrounded = false;
+	                GetComponent<Rigidbody2D>().AddForce(new Vector2(0,10), ForceMode2D.Impulse);
+	            }
+	        }
+	    }
+	    else{
+	    	transform.localScale = new Vector2(-someScale, transform.localScale.y);
+	        
+	        if(Input.GetAxis("P2Hor") > 0.5){
+
+	            Vector2 movingVector = new Vector2(Input.GetAxis("P2Hor") * speed * Time.deltaTime, Input.GetAxis("P2Vert")* 0 * Time.deltaTime);
+
+	            player.transform.Translate(movingVector.x, 0f, 0f);
+	        }
+
+	        if(Input.GetAxis("P2Hor") < -0.5){
+
+	            Vector2 movingVector = new Vector2(Input.GetAxis("P2Hor") * (speed/4) * Time.deltaTime, Input.GetAxis("P2Vert")* 0 * Time.deltaTime);
+
+	            player.transform.Translate(movingVector.x, 0f, 0f);
+	        }
+
+
+	        if(Input.GetAxis("P2Vert") == 1){
+	            if(isGrounded){
+	                isGrounded = false;
+	                GetComponent<Rigidbody2D>().AddForce(new Vector2(0,10), ForceMode2D.Impulse);
+	            }
+	        }
+
+	    }
+
+
+
+
+
+
+
+
+
 
     }
 
